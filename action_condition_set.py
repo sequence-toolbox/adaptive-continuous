76f7a714b4e31f5b4937684486075c2b097d3afd
Alex-Kolar <alexkolar@comcast.net>
2026-07-28 16:41:08 -0500

    Remove redundant swapping protocols

--- action_condition_set.py
+++ action_condition_set.py
@@ -1,20 +1,13 @@
-from typing import List, Tuple, Dict, Any, TYPE_CHECKING
+from typing import TYPE_CHECKING
 
-from sequence.entanglement_management.purification import BBPSSWProtocol
 from sequence.resource_management.memory_manager import MemoryInfo
-
 from sequence.entanglement_management.generation.generation_base import EntanglementGenerationA
-from sequence.entanglement_management.swapping import EntanglementSwappingA, EntanglementSwappingB
-from sequence.resource_management.action_condition_set import Arguments, RequestFunction, ActionReturn, TempMemory, TempNode
-
-from swapping import EntanglementSwappingA_bds, EntanglementSwappingB_bds
+from sequence.resource_management.action_condition_set import Arguments, ActionReturn, TempNode
 
 if TYPE_CHECKING:
     from sequence.components.memory import Memory
     from sequence.entanglement_management.entanglement_protocol import EntanglementProtocol
 
-
-# 1. entanglement generation #
 
 def eg_rule_action_await_adaptive(memories_info: list[MemoryInfo], args: Arguments) -> ActionReturn:
     """Action function used by entanglement generation protocol on nodes except the initiator, i.e., index > 0
@@ -62,57 +55,5 @@ def eg_match_func_adaptive(protocols: list[EntanglementProtocol], args: Argument
         if (isinstance(protocol, EntanglementGenerationA)
                 and protocol.remote_node_name == name
                 and protocol.rule.get_reservation() == reservation):
-            return protocol
-    return None
-
-
-# 3. entanglement swapping #
-
-def es_rule_action_A_adaptive(memories_info: List["MemoryInfo"], args: Arguments) -> Tuple[
-    EntanglementSwappingA | EntanglementSwappingA_bds, List[str], List["es_req_func_adaptive"], List[Dict]]:
-    """Action function used by EntanglementSwappingA protocol on nodes
-    """
-    es_succ_prob = args["es_succ_prob"]
-    memories = [info.memory for info in memories_info]
-    encoding_type = args["encoding_type"]
-
-    if encoding_type == "single_atom":
-        es_degradation = args["es_degradation"]
-        name = "ESA.{}.{}".format(memories[0].name, memories[1].name)
-        protocol = EntanglementSwappingA(None, name, memories[0], memories[1], es_succ_prob, es_degradation)
-    elif encoding_type == 'single_heralded':
-        is_twirled = args['is_twirled']
-        name = "ESA_bds.{}.{}".format(memories[0].name, memories[1].name)
-        protocol = EntanglementSwappingA_bds(None, name, memories[0], memories[1], es_succ_prob, is_twirled)
-
-    dsts = [info.remote_node for info in memories_info]
-    req_funcs = [es_req_func_adaptive, es_req_func_adaptive]
-    req_args = [{"target_memo": memories_info[0].remote_memo}, {"target_memo": memories_info[1].remote_memo}]
-    return protocol, dsts, req_funcs, req_args
-
-
-def es_rule_action_B_adaptive(memories_info: List["MemoryInfo"], args: Arguments) -> Tuple[
-    EntanglementSwappingB | EntanglementSwappingB_bds, List[None], List[None], List[None]]:
-    """Action function used by EntanglementSwappingB protocol
-    """
-    memories = [info.memory for info in memories_info]
-    memory = memories[0]
-    encoding_type = args["encoding_type"]
-    if encoding_type == "single_atom":
-        protocol = EntanglementSwappingB(None, "ESB." + memory.name, memory)
-    elif encoding_type == 'single_heralded':
-        protocol = EntanglementSwappingB_bds(None, "ESB_bds." + memory.name, memory)
-    return protocol, [None], [None], [None]
-
-
-def es_req_func_adaptive(protocols: List["EntanglementProtocol"],
-                         args: Arguments) -> EntanglementSwappingB | EntanglementSwappingB_bds | None:
-    """Function used by `es_rule_actionA` for selecting swapping protocols on the remote node
-    """
-    target_memo = args["target_memo"]
-    for protocol in protocols:
-        if (isinstance(protocol, EntanglementSwappingB | EntanglementSwappingB_bds)
-                # and protocol.memory.name == memories_info[0].remote_memo):
-                and protocol.memory.name == target_memo):
             return protocol
     return None
