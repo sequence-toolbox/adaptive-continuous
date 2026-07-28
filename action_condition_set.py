1cad64598c46daaf4d637a3e5ff4fe0ed2172f04
Alex-Kolar <alexkolar@comcast.net>
2026-07-28 16:13:20 -0500

    Redo purification to match existing BDS formalism

--- action_condition_set.py
+++ action_condition_set.py
@@ -1,13 +1,13 @@
 from typing import List, Tuple, Dict, Any, TYPE_CHECKING
+
+from sequence.entanglement_management.purification import BBPSSWProtocol
 from sequence.resource_management.memory_manager import MemoryInfo
 
 from sequence.entanglement_management.generation.generation_base import EntanglementGenerationA
+from sequence.entanglement_management.swapping import EntanglementSwappingA, EntanglementSwappingB
 from sequence.resource_management.action_condition_set import Arguments, RequestFunction, ActionReturn, TempMemory, TempNode
 
 from swapping import EntanglementSwappingA_bds, EntanglementSwappingB_bds
-from sequence.entanglement_management.swapping import EntanglementSwappingA, EntanglementSwappingB
-from purification import BBPSSW_bds
-from sequence.entanglement_management.purification.bbpssw_circuit import BBPSSWCircuit
 
 if TYPE_CHECKING:
     from sequence.components.memory import Memory
@@ -64,79 +64,6 @@ def eg_match_func_adaptive(protocols: list[EntanglementProtocol], args: Argument
                 and protocol.rule.get_reservation() == reservation):
             return protocol
     return None
-
-
-# 2. entanglement purification #
-
-def ep_rule_action_request_adaptive(memories_info: List["MemoryInfo"], args: Arguments) -> Tuple[
-    BBPSSWCircuit | BBPSSW_bds, List[str], List["ep_match_func_adaptive"], List[Dict]]:
-    """Action function used by BBPSSW protocol on nodes except the initiator node
-    """
-    encoding_type = args["encoding_type"]
-    memories = [info.memory for info in memories_info]
-    if encoding_type == "single_atom":
-        name = "EP.%s.%s" % (memories[0].name, memories[1].name)
-        protocol = BBPSSWCircuit(None, name, memories[0], memories[1])
-    elif encoding_type == "single_heralded":
-        name = "EP_bds.%s.%s" % (memories[0].name, memories[1].name)
-        protocol = BBPSSW_bds(None, name, memories[0], memories[1])
-
-    dsts = [memories_info[0].remote_node]
-    req_funcs = [ep_match_func_adaptive]
-    req_args = [{"remote0": memories_info[0].remote_memo, "remote1": memories_info[1].remote_memo}]
-    return protocol, dsts, req_funcs, req_args
-
-
-def ep_rule_action_await_adaptive(memories_info: List["MemoryInfo"], args: Arguments) -> Tuple[
-    BBPSSWCircuit | BBPSSW_bds, List[None], List[None], List[None]]:
-    """Action function used by BBPSSW protocol on nodes except the responder node
-    """
-    encoding_type = args['encoding_type']
-    memories = [info.memory for info in memories_info]
-    if encoding_type == "single_atom":
-        name = "EP.%s" % memories[0].name
-        protocol = BBPSSWCircuit(None, name, memories[0], None)
-    elif encoding_type == "single_heralded":
-        name = "EP_bds.%s" % memories[0].name
-        protocol = BBPSSW_bds(None, name, memories[0], None)
-    return protocol, [None], [None], [None]
-
-
-def ep_match_func_adaptive(protocols, args: Arguments) -> BBPSSWCircuit | BBPSSW_bds:
-    """Function used by `ep_rule_action1` for selecting purification protocols on the remote node
-       Will "combine" two BBPSSW into one BBPSSW
-
-    Args:
-        protocols (list): a list of waiting protocols
-        args (dict): the arguments
-    Return:
-        the selected protocol
-    """
-    remote0 = args["remote0"]
-    remote1 = args["remote1"]
-
-    _protocols = []
-    for protocol in protocols:
-        if not (isinstance(protocol, BBPSSWCircuit) or isinstance(protocol, BBPSSW_bds)):
-            continue
-
-        if protocol.kept_memo.name == remote0:
-            _protocols.insert(0, protocol)  # 0 is kept
-        if protocol.kept_memo.name == remote1:
-            _protocols.insert(1, protocol)  # 1 is meas
-
-    if len(_protocols) != 2:
-        return None
-
-    protocols.remove(_protocols[1])  # remove the EP_bds for meas
-    _protocols[1].rule.protocols.remove(_protocols[1])
-    _protocols[1].kept_memo.detach(_protocols[1])
-    _protocols[0].meas_memo = _protocols[1].kept_memo  # [0]'s meas is [1]'s kept
-    _protocols[0].memories = [_protocols[0].kept_memo, _protocols[0].meas_memo]
-    _protocols[0].name = _protocols[0].name + "." + _protocols[0].meas_memo.name
-    _protocols[0].meas_memo.attach(_protocols[0])
-
-    return _protocols[0]
 
 
 # 3. entanglement swapping #
